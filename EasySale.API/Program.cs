811c77e1445f9e4e4afb067441b60487fd63a942
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2026-03-16 19:02:05 -0300

    feat: new DI and scopes for main .cs

--- EasySale.API/Program.cs
+++ EasySale.API/Program.cs
@@ -7,6 +7,10 @@ using EasySale.API.UseCases.Caixa.GetById;
 using EasySale.API.UseCases.Caixa.ListarAbertos;
 using EasySale.API.UseCases.Caixa.ListarDisponiveis;
 using EasySale.API.UseCases.Caixa.Register;
+using EasySale.API.UseCases.Caixa.ObterAberturaAtual;
+using EasySale.API.UseCases.Caixa.ConferenciaCegaGet;
+using EasySale.API.UseCases.Caixa.ConferenciaCegaPost;
+using EasySale.API.UseCases.Caixa.RegistrarMovimento;
 using EasySale.API.UseCases.Caixa.Sair;
 using EasySale.API.UseCases.Caixa.Selecionar;
 using EasySale.API.UseCases.Cliente.GetById;
@@ -16,13 +20,21 @@ using EasySale.API.UseCases.Cliente.GetAll;
 using EasySale.API.UseCases.Cliente.Register;
 using EasySale.API.UseCases.Produto.Delete;
 using EasySale.API.UseCases.Produto.GetAll;
+using EasySale.API.UseCases.Produto.GetById;
 using EasySale.API.UseCases.Produto.Register;
+using EasySale.API.UseCases.Produto.Update;
 using EasySale.API.UseCases.Venda.AdicionarItem;
+using EasySale.API.UseCases.Venda.AdicionarPagamento;
+using EasySale.API.UseCases.Venda.AtualizarVenda;
 using EasySale.API.UseCases.Venda.CriarVenda;
 using EasySale.API.UseCases.Venda.FinalizarVenda;
+using EasySale.API.UseCases.Venda.LimparPagamentos;
+using EasySale.API.UseCases.Venda.ListarFormasPagamento;
 using EasySale.API.UseCases.Venda.ListarVendas;
 using EasySale.API.UseCases.Venda.ObterVenda;
 using EasySale.API.UseCases.Venda.RemoverItem;
+using EasySale.API.UseCases.Venda.RemoverVenda;
+using EasySale.API.Entities.Vendas;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -62,7 +74,9 @@ builder.Services.AddScoped<UpdateClienteUseCase>();
 builder.Services.AddScoped<DeleteClienteUseCase>();
 
 builder.Services.AddScoped<GetAllProdutosUseCase>();
+builder.Services.AddScoped<GetProdutoByIdUseCase>();
 builder.Services.AddScoped<RegisterProdutoUseCase>();
+builder.Services.AddScoped<UpdateProdutoUseCase>();
 builder.Services.AddScoped<DeleteProdutoUseCase>();
 
 builder.Services.AddScoped<GetAllCaixasUseCase>();
@@ -74,6 +88,10 @@ builder.Services.AddScoped<SairCaixaUseCase>();
 builder.Services.AddScoped<SelecionarCaixaUseCase>();
 builder.Services.AddScoped<ListarCaixasAbertosUseCase>();
 builder.Services.AddScoped<ListarCaixasDisponiveisUseCase>();
+builder.Services.AddScoped<ObterAberturaAtualUseCase>();
+builder.Services.AddScoped<ConferenciaCegaGetUseCase>();
+builder.Services.AddScoped<ConferenciaCegaPostUseCase>();
+builder.Services.AddScoped<RegistrarMovimentoCaixaUseCase>();
 
 builder.Services.AddScoped<CriarVendaUseCase>();
 builder.Services.AddScoped<AdicionarItemVendaUseCase>();
@@ -81,6 +99,11 @@ builder.Services.AddScoped<ObterVendaUseCase>();
 builder.Services.AddScoped<RemoverItemVendaUseCase>();
 builder.Services.AddScoped<FinalizarVendaUseCase>();
 builder.Services.AddScoped<ListarVendasUseCase>();
+builder.Services.AddScoped<AtualizarVendaUseCase>();
+builder.Services.AddScoped<RemoverVendaUseCase>();
+builder.Services.AddScoped<AdicionarPagamentoVendaUseCase>();
+builder.Services.AddScoped<LimparPagamentosVendaUseCase>();
+builder.Services.AddScoped<ListarFormasPagamentoUseCase>();
 
 var app = builder.Build();
 
@@ -89,6 +112,16 @@ try
     using var scope = app.Services.CreateScope();
     var dbContext = scope.ServiceProvider.GetRequiredService<EasySaleDbContext>();
     dbContext.Database.EnsureCreated();
+    if (!dbContext.FormasPagamento.Any())
+    {
+        dbContext.FormasPagamento.AddRange(
+            new FormaPagamento { Descricao = "Dinheiro", PermiteTroco = true },
+            new FormaPagamento { Descricao = "Cartão de Crédito", PermiteTroco = false },
+            new FormaPagamento { Descricao = "Cartão de Débito", PermiteTroco = false },
+            new FormaPagamento { Descricao = "PIX", PermiteTroco = false }
+        );
+        dbContext.SaveChanges();
+    }
 }
 catch (Exception ex)
 {
