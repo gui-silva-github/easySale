da2f00e1bb61528f21555369b430553700f52415
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2026-03-17 13:59:58 -0300

    feat: routes for 'EasySale' frontend

---
+++ Views/src/routes/index.tsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Layout } from '@/components/layout/Layout';
+import { Dashboard } from '@/pages/Dashboard';
+import { Clients } from '@/pages/clients/Clients';
+import { Products } from '@/pages/products/Products';
+import { Sales } from '@/pages/sales/Sales';
+import { CashRegisters } from '@/pages/cashiers/CashRegisters';
+import { SaleComponent } from '@/pages/sales/Sale';
+
+export const routes = createBrowserRouter([
+    { path: '/', element: <Layout />, children: [
+        { index: true, element: <Dashboard /> },
+        { path: '/clients', element: <Clients /> },
+        { path: '/products', element: <Products /> },
+        { path: '/sales', element: <Sales /> },
+        { path: '/cash-registers', element: <CashRegisters /> },
+        { path: '/sale/:openingId', element: <SaleComponent /> },
+    ] },
+])
